45e0bdb058ad4e13e36568823a8fa016d907bb42
Sunitha123-creator <93782067+Sunitha123-creator@users.noreply.github.com>
2022-02-17 14:49:11 +0530

    Add files via upload

---
+++ Amazon Clone/amazonclone/Address/checkout/customerservice/Displaycontent/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div className="footerarea">
+        <div className="footerarea_top">
+            <p> Back to Top</p>
+        </div>
+        <div className="footerarea_links">
+            <div className="footerarea_linkarea">
+                <span>header1</span>
+
+            </div>
+            <div className="footerarea_linkarea">
+                <span>header2</span>
+
+            </div>
+            <div className="footerarea_linkarea">
+                <span>header3</span>
+
+            </div>
+            <div className="footerarea_linkarea">
+                <span>header4</span>
+
+            </div>
+
+        </div>
+
+        
+        </div>
+    )
+
+}
+export default Footer;
